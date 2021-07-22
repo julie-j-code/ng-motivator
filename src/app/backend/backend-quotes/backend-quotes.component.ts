da07afd97ca08e6f1848a9b890f1dc2d59bb2942
Julie J <jeannet.julie@gmail.com>
2021-07-22 11:27:55 +0200

    permettre à un utilisateur spécifique de faire du CRUD sur firebase

--- src/app/backend/backend-quotes/backend-quotes.component.ts
+++ src/app/backend/backend-quotes/backend-quotes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { QuotesService } from "../../services/quotes.service";
+import { AuthService } from "../../authentification/services/auth.service";
 
 @Component({
   selector: 'app-backend-quotes',
@@ -9,11 +10,24 @@ import { QuotesService } from "../../services/quotes.service";
 export class BackendQuotesComponent implements OnInit {
 
   quotes$;
+  // pour permettre à un seul utilisateur de procéder  au CRUD
+  // il nous faudra ensuite pouvoir accéder au service qui dispose de l'observable user$
+  // on pourra ensuite utiliser une directive structurelle côté template *ngIf="isAdmin"
+  isAdmin:boolean = false;
 
-  constructor(private s:QuotesService) { }
+
+  constructor(private s:QuotesService, private authService:AuthService ) { }
 
   ngOnInit(): void {
     this.quotes$=this.s.getQuotes();
+    this.authService.user$.subscribe(user=>{
+      console.log("user", user);
+      if (user && user.uid==="G7U8gEs8w3SZ0A3jkllkgaJ3ZcT2"){
+        this.isAdmin = true;
+      }else{
+        this.isAdmin = false;
+      }
+    })
   }
 
   deleteQuote(quote){
