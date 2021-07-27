7869596634e64462d4f0c3f4edb494c80c6f59c5
Julie J <jeannet.julie@gmail.com>
2021-07-27 09:50:09 +0200

    substitution méthodes de récupération des valeurs de détails : angularFireDatabase.objet au lieu de angularFireDatabase.database.ref. Impeccable

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { AngularFireDatabase, AngularFireDatabaseModule, AngularFireObject } from '@angular/fire/database';
 // pour récupérer les paramètres d'une route
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Quote } from '../models/quote.model';
 
 
 @Component({
@@ -12,9 +14,10 @@ import { ActivatedRoute } from '@angular/router';
 export class QuoteDetailsComponent implements OnInit {
 
   quoteId;
-  text;
-  firstname;
-  lastname;
+  // text;
+  // firstname;
+  // lastname;
+  item;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -26,16 +29,26 @@ export class QuoteDetailsComponent implements OnInit {
     // pour être cohérent, faut que le paramètre dynamique qu'on récupère soit le même que celui de la route
     this.quoteId = this.activatedRoute.snapshot.paramMap.get('id');
 
-    return this.afDb.database.ref('/quotes').orderByChild('key').equalTo(this.quoteId).once('value', snapshot => {
-      snapshot.forEach(childSnapshot => {
-        var childData = childSnapshot.val();
-        this.firstname = childData.firstname;
-        this.lastname = childData.lastname;
-        this.text = childData.text;
-        console.log('text', this.text);
-      });
+    // méthode initiale
+    // return this.afDb.database.ref('/quotes').orderByChild('key').equalTo(this.quoteId).once('value', snapshot => {
+    //   snapshot.forEach(childSnapshot => {
+    //     var childData = childSnapshot.val();
+    //     this.firstname = childData.firstname;
+    //     this.lastname = childData.lastname;
+    //     this.text = childData.text;
+    //     console.log('text', this.text);
+    //   });
+
+    // nouvel essai autre méthode : la bonne
+    // beaucoup moins verbeuse !
+    this.afDb.object('quotes/' + this.quoteId).valueChanges().subscribe(val => {
+      this.item = val;
     });
+
   };
+
+
+
 
 
 }
